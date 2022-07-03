dc75fe590da1b0411fb74c27ae2af34219abeb32
Dev E.B <deveb1479@gmail.com>
2022-07-03 15:29:30 +0900

    in now, must make Library class, main library system

--- LibraryProject/Controls/BarcodeScanner.cs
+++ LibraryProject/Controls/BarcodeScanner.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Excel = Microsoft.Office.Interop.Excel;
+
 namespace LibraryProject.Controls
 {
     /// <summary>
@@ -12,6 +14,8 @@ namespace LibraryProject.Controls
     /// </summary>
     internal class BarcodeScanner : RichTextBox
     {
+        private const int TITLE = 1, DESCRIPTION = 2, AUTHOR = 3, BOOKCODE = 4, BARCODE = 5;
+
         /// <summary>
         /// 컨트롤의 기본 생성자입니다.
         /// </summary>
@@ -22,12 +26,29 @@ namespace LibraryProject.Controls
 
         /// <summary>
         /// 텍스트 박스 내 입력된, 책의 바코드를 기반으로 같은 바코드를 지닌 책의 정보를 찾습니다.
+        /// 책을 찾지 못했다면 null을 반환합니다.
         /// </summary>
         /// <param name="path"> 엑셀 데이터베이스의 상대 경로입니다. </param>
         /// <returns></returns>
-        public Structure.Book Find(string path)
+        public Structure.Book FindBook(string path)
         {
-            return null; // editting point
+            Excel.Application app = new Excel.Application();
+            Excel.Workbook workbook = app.Workbooks.Open(path);
+            Excel.Worksheet worksheet = workbook.Worksheets[0];
+
+            for (int r = 0; r < worksheet.Rows.Count; r++)
+            {
+                if (worksheet.Cells[r, BARCODE].Value == this.Text)
+                {
+                    return new Structure.Book(worksheet.Cells[r, TITLE].Value,
+                                              worksheet.Cells[r, DESCRIPTION].Value,
+                                              worksheet.Cells[r, AUTHOR].Value,
+                                              worksheet.Cells[r, BOOKCODE].Value,
+                                              worksheet.Cells[r, BARCODE].Value);
+                }
+            }
+
+            return null;
         }
     }
 }
---
+++ LibraryProject/Structure/Library.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryProject.Structure
+{
+    /// <summary>
+    /// 도서관의 메인 중앙 시스템 정보 클래스입니다.
+    /// </summary>
+    internal class Library
+    {
+
+    }
+}
