e8e088efb5fe6fb9cb85312cdd9dfa0b7fce0779
Dev E.B <deveb1479@gmail.com>
2022-07-12 12:26:17 +0900

    edit assembly

--- Project-Library/Properties/AssemblyInfo.cs
+++ Project-Library/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("LibraryProject")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("My Small Library")]
+[assembly: AssemblyDescription("My own little library made with my own hands")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("LibraryProject")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyCompany("CS WAVE")]
+[assembly: AssemblyProduct("Project-Library")]
+[assembly: AssemblyCopyright("Copyright ©  2022 elecbug")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
